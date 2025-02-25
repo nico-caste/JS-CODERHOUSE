f3f7170cb870327f6e7210396a9ccf6eb5f52374
NicoCastellanos <nicocastellanoslive.com.ar>
2025-02-24 21:14:05 -0300

    arreglo calificacion

--- js/main.js
+++ js/main.js
@@ -5,32 +5,40 @@ document.body.insertBefore(titulo, document.body.childNodes[0]);
 let alumnos = [];
 
 function ingresaralumno() {
-    const input = document.getElementById('name');
-    const nombre = input.value.trim();
+    let inputn = document.getElementById('name');
+    let nombre = inputn.value.trim ();
     
     if (nombre === '') {
         alert('Por favor ingrese un nombre correcto');
         return;
     }
-    alumnos.push(nombre);
-    /*limpiar input*/
-    input.value = '';
-    actualizarLista();
+    
+    let inputc = document.getElementById('cal');
+    let calificacion = parseInt(inputc.value);
+    if (calificacion < 0 || calificacion > 10) {
+        alert (`por favor, ingrese una calificacion correcta`);
+        return;
+    }
+    alumnos.push (nombre + calificacion);
+    inputc.value = ``;
+    inputn.value = '';
+    actualizarlista();
+
 }
 
-function actualizarLista() {
-    const lista = nuevoalumnolista();
+function actualizarlista() {
+    let lista = nuevoalumnolista();
     lista.innerHTML = '';
     
     alumnos.forEach(alumno => {
-        const li = document.createElement('li');
+        let li = document.createElement('li');
         li.textContent = alumno;
         lista.appendChild(li);
     });
 }
 
 function nuevoalumnolista() {
-    const lista = document.createElement('ul');
+    let lista = document.createElement('ul');
     lista.id = 'lista-alumnos';
     document.body.appendChild(lista);
     return lista;
@@ -39,4 +47,4 @@ function nuevoalumnolista() {
 let boton = document.getElementById('btn');
 boton.addEventListener("click", ingresaralumno);
 
-actualizarLista();
+actualizarlista();
